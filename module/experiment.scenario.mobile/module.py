1c6b2441756b8fc2b2866cfb187abb4622f774a1
Grigori Fursin <grigori.fursin@cTuning.org>
2017-03-06 16:01:28 +0100

    sorting scenarios by title ...

--- module/experiment.scenario.mobile/module.py
+++ module/experiment.scenario.mobile/module.py
@@ -151,9 +151,10 @@ def get(i):
 
                 nlst.append(q)
 
-    # Sort by size
+    # Sort by title
     snlst=[]
-    for q in sorted(nlst, key=lambda x: (x.get('total_file_size',0),x.get('meta',{}).get('title',''))):
+#    for q in sorted(nlst, key=lambda x: (x.get('total_file_size',0),x.get('meta',{}).get('title',''))):
+    for q in sorted(nlst, key=lambda x: x.get('meta',{}).get('title','')):
         snlst.append(q)
 
 #    ck.save_json_to_file({'json_file':'/tmp/xyz888.json','dict':nlst})
