996be653e4ff49ad660e465d58264d6fc3d2d214
Grigori Fursin <grigori.fursin@cTuning.org>
2017-03-30 17:03:46 +0200

    fixing minor bug

--- module/experiment.bench.dnn.mobile/module.py
+++ module/experiment.bench.dnn.mobile/module.py
@@ -513,7 +513,7 @@ def show(i):
            tmax=extra.get('xopenme_execution_time_kernel_'+str(ixo)+'_max',0)
 
            xx='<b>'+('%.3f'%tmin)+'</b>&nbsp;/&nbsp;'+('%.3f'%tmax)
-           if tmin==0 and ixo!=2: 
+           if tmin==0 and ixo!=1: 
               xx+='<br><b><center>bug?</center></b>\n'
 
            h+='   <td '+ha+' '+bgx1+'>'+xx+'</a></td>\n'
